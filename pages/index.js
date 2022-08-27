38ee8fe3e01d38243efbf793507f67dc0c094db1
baltazarparra <baltazarparra@outlook.com>
2022-08-27 14:40:11 -0300

    start type search

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,30 @@
+import { useState, useEffect } from 'react'
 import { getProviders, getSession, useSession } from 'next-auth/react'
 
 import Head from 'next/head'
+import Image from 'next/image'
 import Login from '../components/Login'
+
+const fakeApi = () => console.log('Api is called')
 
 export default function Home({ providers }) {
   const { data: session } = useSession()
+  const [inputValue, setInputValue] = useState('')
+  const [searchTerm, setSearchTerm] = useState()
 
   console.log('session', session)
 
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setSearchTerm(inputValue)
+    }, 800)
+
+    return () => clearTimeout(timer)
+  }, [inputValue])
+
   if (!session) return <Login providers={providers} />
+
+  console.log('inputValue', inputValue)
 
   return (
     <div>
@@ -21,10 +37,22 @@ export default function Home({ providers }) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div>
-        <h1>O que jogar?</h1>
-        <img src={session.user.image} alt={session.user.name} />
-        <p>{session.user.name}</p>
-        <p>{session.user.email}</p>
+        <h1>Adicionar Jogos</h1>
+        <Image
+          width="100px"
+          height="100px"
+          src={session.user.image}
+          alt={session.user.name}
+        />
+        <div>
+          <input
+            placeholder="Procurar jogos"
+            value={inputValue}
+            type="text"
+            onChange={(e) => setInputValue(e.target.value)}
+          />
+          {searchTerm && <p>{searchTerm}</p>}
+        </div>
       </div>
     </div>
   )
