938322fdd092c176e4c139674728be6a7ac34a6e
baltazarparra <baltazarparra@outlook.com>
2022-08-31 13:09:54 -0300

    update style

--- pages/_app.js
+++ pages/_app.js
@@ -2,23 +2,21 @@ import { SessionProvider } from 'next-auth/react'
 import { createGlobalStyle } from 'styled-components'
 
 const GlobalStyle = createGlobalStyle`
-html{
-  box-sizing: border-box;
-  background: #F5F4F0;
-  display:block;
-  height: 100%;
-  max-width: 640px;
-  margin:0 auto;
-  padding: 0;
-}
+  html {
+    box-sizing: border-box;
+    background-color: #BFC3CD;
+    display: block;
+    height: 100%;
+    max-width: 728px;
+    margin: 0 auto;
+    padding: 0;
+  }
 
-body{
-  background-color:#fafafa;
-  min-height:100vh;
-  padding: 1rem;
-  margin-top:0;
-  font-family: 'Roboto Mono', monospace;
-}
+  body {
+    padding: 1rem;
+    margin-top: 0;
+    font-family: 'Roboto Mono', monospace;
+  }
 `
 
 export default function MyApp({
