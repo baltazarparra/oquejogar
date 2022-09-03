5aefdc5208c5e32ab6cd258776c81dbb8878d138
baltazarparra <baltazarparra@outlook.com>
2022-09-03 00:03:25 -0300

    add shadow to login

--- components/styles.js
+++ components/styles.js
@@ -51,6 +51,13 @@ export const Button = styled.button`
   border-radius: 3px;
   border: solid 1px #192534;
   cursor: pointer;
+  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
+  transition: all 0.3s cubic-bezier(0.25, 0.8, 0.25, 1);
+  transition: 0.3s;
+
+  &:hover {
+    box-shadow: 0 14px 28px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.22);
+  }
 
   svg {
     margin-right: 10px;
